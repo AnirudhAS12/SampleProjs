918dd24cb13f82b166c74fc87e65ccf46e25b97f
Anirudh Srinath <m1042946@mindtree.com>
2018-02-06 08:16:05 +0530

    cricassign

--- src/App.js
+++ src/App.js
@@ -4,11 +4,27 @@ import './App.css';
 import Header from './components/header/js/header';
 import Main from './components/maincomponent/js/main';
 class App extends Component {
+  constructor()
+  {
+    super();
+   this.state={
+     flag : true
+   }
+  }
+  handle=()=>
+  {
+    this.setState({
+      flag:false
+    })
+  }
   render() {  
     return (
       <div className="App">
         <Header />
         <Main/> 
+        <button id="odi" className="btn btn-primary" onClick={this.handle}>
+          ODI
+        </button>
       </div>
     );
   }
--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+//1.
+import {shallow} from 'enzyme';
+
+// it('renders without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<App />, div);
+//   ReactDOM.unmountComponentAtNode(div);
+// });
+
+//2.describe - grouping all the test cases together
+describe('<App/>',()=>{
+  //3,. it for one particular test case and a random message
+  it('renders div' , ()=>{
+
+    //4.shallow - shallow copy of that component
+    const a=shallow(<App/>);
+
+    //5.expect - what output is expected
+    expect(a.find('div.App').length).toEqual(1)
+  });
+  
+
+  it('testing button functionality', ()=>
+  {
+    const b=shallow(<App />);
+
+    var but=b.find('#odi');
+    but.simulate('click');
+    expect(b.state().flag).toEqual(false);
+
+  })
 });
